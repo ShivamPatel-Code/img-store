eb9d86ed0130ea03e95437bda9ad98044aa22494
Shivam Patel <shivampatelcodes01@gmail.com>
2025-02-20 15:50:24 -0500

    Initialize the project

---
+++ src/test/java/com/syfproject/img_store/ImgStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syfproject.img_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
