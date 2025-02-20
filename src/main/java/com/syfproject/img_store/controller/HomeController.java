180fa94ddbe9d614c248b4acebdc3a3a56fb3684
Shivam Patel <shivampatelcodes01@gmail.com>
2025-02-20 16:35:45 -0500

    set up login with github oauth2

---
+++ src/main/java/com/syfproject/img_store/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.syfproject.img_store.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Img Store";
+    }
+
+}
